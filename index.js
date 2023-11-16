088f4ca836255b682337fc4360c5015bceaf70c0
user name <user email>
2023-11-15 23:20:38 -0500

    changes to functions

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+
+
+function newImage(src,positionLeft, positionBottom){
+    let image = document.createElement('img');
+    image.src = src;
+    image.style.position = 'fixed';
+    image.style.left = positionLeft;
+    image.style.bottom = positionBottom;
+    document.body.append(image);
+    
+    }
+    
+    newImage('assets/pine-tree.png', '450px', '200px');
+    newImage('assets/green-character.gif', '100px', '100px');
+    newImage('assets/tree.png', '200px', '300px');
+    newImage('assets/pillar.png','350px', '200px');
+    newImage('assets/crate.png', '150px','200px');
+    newImage('assets/well.png', '500px', '425px');
+
+    
+    
+    function newItem(src, positionLeft, positionBottom) {
+        let item = document.createElement('img');
+        item.src = src;
+        item.style.position = 'fixed';
+        item.style.left = positionLeft;
+        item.style.bottom = positionBottom;
+        document.body.append(item);
+      
+        item.addEventListener('dblclick', function() {
+          item.remove();
+        });
+      }
+    
+    
+newItem('assets/sword.png', '500px', '405px');
+newItem('assets/shield.png','165px','100px');
+newItem('assets/staff.png' ,'600px','100px');
